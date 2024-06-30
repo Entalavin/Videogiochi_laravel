0e521728830e115b1cf5aa57597d0c3611fb1920
Entalavin <vale.demontis24@live.it>
2024-06-30 12:33:40 +0200

    progetto finito

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,5 +4,9 @@
 
 @section('content')
 
-<h1 class="text-center text-uppercase">Da qui gestisci il database</h1>
+<div class="container">
+    <h1 class="text-center text-uppercase">Da qui gestisci il database</h1>
+    <img class="img-fluid" src="/img/sfondodashboard.jpg" alt="">
+</div>
+
 @endsection
